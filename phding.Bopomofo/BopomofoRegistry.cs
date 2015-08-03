beb3e85daa4375cb88a18de197c54d9d18345fed
Philip Ding <gjding.philip@gmail.com>
2015-08-03 10:52:39 +0800

    initial commit

---
+++ phding.Bopomofo/BopomofoRegistry.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace phding.Bopomofo
+{
+    public class BopomofoRegistry
+    {
+        private static RegistryKey GetSimplifiedKey()
+        {
+            RegistryKey key = Registry.CurrentUser;
+            key = key.OpenSubKey(@"Software\Microsoft\IME\15.0\IMETC", true);
+            return key;
+        }
+
+
+        public static bool IsSimplifiedEnable()
+        {
+            var key = GetSimplifiedKey();
+            if (key == null)
+            {
+                throw new KeyNotFoundException("Can't find IME key");
+            }
+
+            var val = key.GetValue("Enable Simplified Chinese Output");
+            if ((string)val == "0x00000000")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static void EnableSimplified(bool val)
+        {
+            var key = GetSimplifiedKey();
+            if (key == null)
+            {
+                throw new KeyNotFoundException("Can't find IME key");
+            }
+
+            var setValue = val ? "0x00000001" : "0x00000000";
+
+            key.SetValue("Enable Simplified Chinese Output", setValue);
+
+        }
+    }
+}
---
+++ phding.input/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using phding.Bopomofo;
+
+namespace phding.input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (BopomofoRegistry.IsSimplifiedEnable())
+            {
+                Console.WriteLine("Disable");
+                BopomofoRegistry.EnableSimplified(false);
+            }
+            else
+            {
+                Console.WriteLine("Enable");
+                BopomofoRegistry.EnableSimplified(true);
+            }
+        }
+    }
+}
