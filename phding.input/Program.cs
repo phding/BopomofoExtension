3347414bbb69201b807872dada50a27daee89c76
Philip Ding <gjding.philip@gmail.com>
2015-08-03 11:53:49 +0800

    Runnable version

--- phding.input/Program.cs
+++ phding.input/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Forms;
+
 using phding.Bopomofo;
 
 namespace phding.input
@@ -11,7 +13,29 @@ namespace phding.input
     {
         static void Main(string[] args)
         {
+            // Check if already running
+            var title = ((AssemblyTitleAttribute)Assembly.GetExecutingAssembly().GetCustomAttribute(typeof(AssemblyTitleAttribute))).Title;
 
+            var processCount = Process.GetProcesses().Count(p => p.ProcessName == title);
+            
+            if (processCount > 1)
+            {
+                Console.WriteLine("Already exist");
+                return;
+            }
+
+            //Register
+            
+            HotKeyManager.RegisterHotKey(Keys.S, KeyModifiers.Alt, KeyModifiers.Control);
+            HotKeyManager.HotKeyPressed += HotKeyManager_HotKeyPressed;
+            while (true)
+            {
+                Thread.Sleep(1000000);
+            }
+        }
+
+        private static void HotKeyManager_HotKeyPressed(object sender, HotKeyEventArgs e)
+        {
             if (BopomofoRegistry.IsSimplifiedEnable())
             {
                 Console.WriteLine("Disable");
--- phding.input/Properties/AssemblyInfo.cs
+++ phding.input/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("phding.input")]
+[assembly: AssemblyTitle("BopomofoExtension")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("phding.input")]
+[assembly: AssemblyCompany("Phding")]
+[assembly: AssemblyProduct("BopomofoExtension")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
